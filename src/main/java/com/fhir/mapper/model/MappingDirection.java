e7f8df97be9f55414ef262771421e66f13d64341
pradeepkk321 <pradyskumar@gmail.com>
2025-11-19 02:14:05 -0500

    project restructuring

--- src/main/java/com/fhir/mapper/model/MappingDirection.java
+++
@@ -1,9 +0,0 @@
-package com.fhir.mapper.model;
-
-/**
- * Mapping direction enum
- */
-public enum MappingDirection {
-    JSON_TO_FHIR,
-    FHIR_TO_JSON
-}
--- src/main/java/com/fhir/mapper/validation/MappingValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.fhir.mapper.validation;
-
-public class MappingValidationException extends RuntimeException {
-    public MappingValidationException(String message) {
-        super(message);
-    }
-}
